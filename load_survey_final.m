cc992dd107e5cbaef6de7f840a371b6961bffd22
mayercl <45677030+mayercl@users.noreply.github.com>
2023-05-03 17:02:47 -0400

    Add files via upload

--- load_survey_final.m
+++ load_survey_final.m
@@ -3,7 +3,9 @@ close all;
 
 %% load the survey data and update subject.mat
 load('subject.mat');
+save_data = 'off';
 
+% store the subject ids from data structure
 for j = 1:length(subject)
 
     subject_names_all(j) = subject(j).name;
@@ -14,7 +16,7 @@ filepath = '/Users/calebmayer/Dropbox (University of Michigan)/sync project/surv
 patients_real = readtable('/Users/calebmayer/Dropbox (University of Michigan)/sync project/patients_real.csv','HeaderLines',0,'VariableNamingRule','preserve');
 patients_real = [table(patients_real.Properties.VariableNames(1),patients_real.Properties.VariableNames(2),'VariableNames',{patients_real.Properties.VariableNames{1},patients_real.Properties.VariableNames{2}}); patients_real];
 patients_real = renamevars(patients_real, '726a9509fdc6adfde7d693480b16896c', 'Var2');
-names_all = patients_real.Var2;
+names_all = patients_real.Var2; % subject ids from file
 
 daily_empty = [];
 weekly_empty = [];
@@ -31,7 +33,8 @@ for j = 1:length(names_all)
         daily_results = readtable([filepath names_all{j} '_daily.xls']);
 
         if ~isempty(subject_index)
-
+            
+            % store the daily results 
             subject(subject_index).daily_results = daily_results;
 
         else 
@@ -42,7 +45,7 @@ for j = 1:length(names_all)
 
     catch 
 
-        daily_empty = [daily_empty; j];
+        daily_empty = [daily_empty; j]; % store indices where we don't have results
 
     end 
 
@@ -51,7 +54,8 @@ for j = 1:length(names_all)
         weekly_results = readtable([filepath names_all{j} '_weekly.xls']);
 
         if ~isempty(subject_index)
-
+            
+            % store the weekly results
             subject(subject_index).weekly_results = weekly_results;
 
         else 
@@ -62,7 +66,7 @@ for j = 1:length(names_all)
 
     catch 
 
-        weekly_empty = [weekly_empty; j];
+        weekly_empty = [weekly_empty; j]; % store indices where we don'thave results
 
     end 
 
@@ -71,7 +75,8 @@ for j = 1:length(names_all)
         pre_results = readtable([filepath names_all{j} '_pre.xls']);
 
         if ~isempty(subject_index)
-
+            
+            % store pre results
             subject(subject_index).pre_results = pre_results;
 
         else 
@@ -82,7 +87,7 @@ for j = 1:length(names_all)
 
     catch 
 
-        pre_empty = [pre_empty; j];
+        pre_empty = [pre_empty; j]; % store indices where we don't have results
 
     end 
 
@@ -91,7 +96,8 @@ for j = 1:length(names_all)
         post_results = readtable([filepath names_all{j} '_post.xls']);
 
         if ~isempty(subject_index)
-
+            
+            % store post results
             subject(subject_index).post_results = post_results;
 
         else 
@@ -102,10 +108,15 @@ for j = 1:length(names_all)
 
     catch
 
-        post_empty = [post_empty; j];
+        post_empty = [post_empty; j]; % store indices where we don't have results
 
     end 
 
 end 
 
-save('subject.mat','subject.mat');
+% save the results 
+if strcmp(save_data,'on')
+
+    save('subject.mat','subject.mat');
+
+end 
