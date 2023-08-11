73e23ab9a0440964463903920d6d23e29145d714
mayercl <45677030+mayercl@users.noreply.github.com>
2023-08-11 12:48:39 -0400

    Update to include supplemental information

--- main_survey_paper.m
+++ main_survey_paper.m
@@ -3,6 +3,7 @@ close all;
 
 %% load the data
 %read_survey_final; % read the surveys into survey_formatted_v2
+
 load_survey_final; % load the survey data 
 
 % process the survey data
@@ -19,13 +20,37 @@ tscore_website_format; % then use website
 weekly_survey_analysis;
 
 % analyze the daily data 
-daily_surveys_results
+daily_surveys_results_final;
+
+% analyze pre/post fatigue
+pre_post_fatigue_analysis;
 
 % analyze the pre/post data 
-tscore_results_analyze;
+tscore_imputed_results_analyze;
 
 % set up the data for gee analysis  
 gee_model_main;
 
 % analyze imputed datasets
 imputed_dataset_analysis;
+
+% portion of demographics table
+demographics_weekly;
+
+%% supplemental information 
+% missingness
+missing_data_checks;
+
+weekly_daily_survey_analysis_supplement;
+
+% basic wearable metrics--daily avg steps, hr 
+basic_wearables_analysis; 
+
+% time of day and daily surveys 
+surveys_time_of_day;
+
+% analyze the pre/post data 
+tscore_results_analyze;
+
+% weekly vs daily surveys 
+daily_weekly_fatigue_comp;
